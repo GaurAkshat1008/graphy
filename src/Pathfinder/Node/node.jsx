61484775beb439c7b1337fd66ba9bf3b23459956
Akshat <akshatgauriiitsurat@gmail.com>
2022-06-05 01:44:35 +0530

    5/6/2022

--- src/Pathfinder/Node/node.jsx
+++ src/Pathfinder/Node/node.jsx
@@ -1,14 +1,45 @@
-import React, {Component} from 'react'
-import './node.css'
+import React, { Component } from "react";
+import "./node.css";
 
-export default class Node extends Component{
-  constructor(props){
-    super(props)
-    this.state = {}
+export default class Node extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
-  render(){
+  render() {
+    const {
+      row,
+      col,
+      isStart,
+      isFinish,
+      isWall,
+      onMouseDown,
+      onMouseUp,
+      onMouseEnter,
+    } = this.props;
+    const extraClassName = isFinish
+      ? "node-finish"
+      : isStart
+      ? "node-start"
+      : isWall
+      ? "node-wall"
+      : "";
     return (
-        <div className="node"></div>
-    )
+      <div
+        id={`node-${row}-${col}`}
+        className={`node ${extraClassName}`}
+        onMouseDown={() => {
+          onMouseDown(row, col);
+        }}
+        onMouseEnter={() => {
+          onMouseEnter(row, col);
+        }
+        }
+        onMouseUp={() => {
+          onMouseUp();
+        }
+        }
+      ></div>
+    );
   }
 }
--- src/Pathfinder/visualizer.jsx
+++ src/Pathfinder/visualizer.jsx
@@ -3,36 +3,103 @@ import Header from "../component/header";
 import Node from "./Node/node";
 import "./visualizer.css";
 
+const START_ROW = 8
+const START_COL = 20
+const FINISH_COL = 30
+const FINISH_ROW = 8
+
+function createNode(row, col) {
+  return {
+    row,
+    col,
+    isStart: row === START_ROW && col === START_COL,
+    isFinish: row === FINISH_ROW && col === FINISH_COL,
+    distance: Infinity,
+    isVisited: false,
+    isWall: false,
+    previousNode: null,
+  };
+}
+
+function getGrid() {
+  const grid = [];
+  for (let row = 0; row < 15; row++) {
+    const currentRow = [];
+    for (let col = 0; col < 50; col++) {
+      currentRow.push(createNode(row, col));
+    }
+    grid.push(currentRow);
+  }
+  return grid;
+}
+
+function getNewGridWithWall(grid, row, col) {
+  const newGrid = grid.slice();
+  const node = newGrid[row][col];
+  const newNode = {
+    ...node,
+    isWall: true,
+  };
+  newGrid[row][col] = newNode;
+  return newGrid;
+}
+
 export default class Visualizer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      nodes: [],
+      grid: [],
+      mousePressed: false,
     };
   }
   componentDidMount() {
-    const cells = [];
-    for (let row = 0; row < 15; row++) {
-      const currentRow = [];
-      for (let col = 0; col < 50; col++) {
-        currentRow.push([]);
-      }
-      cells.push(currentRow);
-    }
-    this.setState({ nodes: cells });
+    const grid = getGrid();
+    this.setState({ grid: grid });
   }
+
+  handleMouseDown(row, col) {
+    const newGrid = getNewGridWithWall(this.state.grid, row, col);
+    this.setState({ mousePressed: true, grid: newGrid });
+  }
+
+  handleMouseEnter(row, col) {
+    if (!this.state.mousePressed) return;
+    const newGrid = getNewGridWithWall(this.state.grid, row, col);
+    this.setState({ grid: newGrid });
+  }
+
+  handleMouseUp() {
+    this.setState({ mousePressed: false });
+  }
+
   render() {
-    const { nodes } = this.state;
-    console.log(nodes);
+    const { grid, mousePressed } = this.state;
+    console.log(grid);
     return (
       <div className="visualizer">
         <Header />
         <div className="grid">
-          {nodes.map((row, rowIdx) => {
+          {grid.map((row, rowIdx) => {
             return (
               <div key={rowIdx}>
                 {row.map((node, nodeIdx) => {
-                  return <Node key={nodeIdx} />;
+                  const { row, col, isStart, isFinish, isWall } = node;
+                  return (
+                    <Node
+                      key={nodeIdx}
+                      col={col}
+                      row={row}
+                      isStart={isStart}
+                      isFinish={isFinish}
+                      isWall={isWall}
+                      mousePressed={mousePressed}
+                      onMouseDown={(row, col) => this.handleMouseDown(row, col)}
+                      onMouseEnter={(row, col) =>
+                        this.handleMouseEnter(row, col)
+                      }
+                      onMouseUp={() => this.handleMouseUp()}
+                    />
+                  );
                 })}
               </div>
             );
